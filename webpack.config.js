1a4756ba7203a78005a757e0e9a12de76a1efebc
Vox1oot <voxfoot@gmail.com>
2022-11-30 16:23:13 +0500

    config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require('path');
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode,
+  devServer: {
+    open: true,
+  },
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+      test: /\.html$/i,
+      loader: "html-loader",
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader",],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        },
+      },
+      {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name][ext]'
+      },
+      },
+    ],
+  },
+};
