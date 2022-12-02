a9a65ef6f5dfb333dc8a82d58898be24b9f8cb3a
Vox1oot <voxfoot@gmail.com>
2022-12-02 14:12:35 +0500

    begin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,2 @@
-const toggleBurger = (burgerElement, buttonsElement) => {
-  /* придумать как навешать стили на бургер чтобы он не ломал верстку */
-}
 
-export default () => {
-  const menuButtonsEl = document.querySelector('.header__buttons');
-  const burgerButton = document.querySelector('.header__burger');
-
-  toggleBurger(burgerButton, menuButtonsEl);
-};
 
