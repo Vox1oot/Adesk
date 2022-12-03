68dce87c97b1a3552997209eda4add5e40140de3
morsa <voxfoot@gmail.com>
2022-12-03 22:35:37 +0500

    implement desktop layout

--- src/app.js
+++ src/app.js
@@ -1,2 +1,25 @@
+const toggle = (elem) => {
+  if (elem.classList.contains('hidden')) {
+    elem.classList.replace('hidden', 'flex');
+    elem.scrollIntoView({block: "center", behavior: "smooth"});
+    return;
+  }
 
+  setTimeout(() => {
+    elem.classList.replace('flex', 'hidden');
+  }, 100);
+};
+
+export default () => {
+  const buttons =  document.querySelectorAll('.questions__btn');
+
+  Array.from(buttons).forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      const answerElement = event.target.nextSibling.nextSibling;
+      
+      toggle(answerElement);
+    })
+  });
+};
 
